f94d6b90825c8b4724b2af78476b07215a0cb39e
Naveen <sheoran19997@gmail.com>
2023-10-18 16:47:16 +0530

    integerate mailing service

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 
 const sequelize = require('./util/database');
 const authroutes = require('./routes/authroutes');
+const forgotpasswordroutes = require('./routes/forgotpassword');
 const expenseroutes = require('./routes/expenseroutes');
 const purchaseroutes = require('./routes/purchase');
 const premiumroutes = require('./routes/premium');
@@ -19,6 +20,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use(authroutes);
+app.use(forgotpasswordroutes);
 app.use(expenseroutes);
 app.use(purchaseroutes);
 app.use(premiumroutes);
