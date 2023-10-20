aa684da281824c049e0f163fbafcf06653c510d9
Naveen <sheoran19997@gmail.com>
2023-10-20 12:47:24 +0530

    download expenses

--- controllers/premium.js
+++ controllers/premium.js
@@ -1,6 +1,9 @@
 const User = require('../models/user')
 const Expense = require('../models/expenses')
 const sequelize = require('../util/database')
+const AWS =require('aws-sdk');
+const UserServices = require('../services/userservices');
+const S3services = require('../services/s3services');
 
 exports.showleaderboard = async (req,res,next)=>{
     try{
@@ -36,4 +39,21 @@ exports.showleaderboard = async (req,res,next)=>{
         res.status(500).json(err);
     }
 
+}
+
+
+
+exports.download = async(req,res,next)=>{
+    try{
+        const userid = req.user.id ;
+        const expenses = await UserServices.getExpenses(req);
+        const stringifiedExpenses = JSON.stringify(expenses);
+        const filename =`Expenses${userid}/${new Date()}.txt`;
+        const fileUrl = await S3services.uploadtoS3(stringifiedExpenses,filename);
+        res.status(200).json({fileUrl , success : true});
+    }catch(err){
+        console.log(err);
+        res.status(500).json({fileUrl : '' , success : false , err : err} )
+    }
+    
 }
--- routes/premium.js
+++ routes/premium.js
@@ -6,4 +6,6 @@ const router = express.Router();
 
 router.get('/premium/showleaderboard', authmiddleware.authenticate ,premiumController.showleaderboard);
 
+router.get('/user/download', authmiddleware.authenticate , authmiddleware.checkpremium ,premiumController.download);
+
 module.exports = router ;
